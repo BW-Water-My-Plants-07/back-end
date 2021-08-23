ae3d4650c972cfdba6b5d9d70e03653b2dba0668
Nora C <nocorser@gmail.com>
2021-08-23 15:33:33 -0500

    start server + file hierarchy setup

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const helmet = require('helmet')
 const cors = require('cors')
-const db = require('./data/db-config')
+const helmet = require('helmet')
+const db = require('../data/db-config')
 
 function getAllUsers() { return db('users') }
 
