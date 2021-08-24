afa05cfef0a84d8c99253c0b705d20e98654952b
Nora C <nocorser@gmail.com>
2021-08-24 13:37:38 -0500

    Add plant

--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -5,16 +5,23 @@ function findByUserId(user_id) {
     return db('plants').where('user_id', user_id)
 }
 
-// Find plant by User ID
-function findById(id) {
-    return db('plants').where('plant_id', id)
+// Find a user's plant by plant ID
+function findById(id, user_id) {
+    return db('plants')
+        .where({'plant_id':id, 'user_id':user_id})
 }
 
 // resolves to the newly inserted plant
 async function add(plant) {
-    const [id] = await db('plants').insert(plant)
-    return findById(id)
+    const [id] = await db('plants').insert(plant, ['plant_id', 'user_id'])
+    return findById(id.plant_id, id.user_id)
 }
+
+// TODO edit
+
+
+// TODO delete
+
 
 module.exports = {
     findByUserId,
--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Plants = require("../plants/plants-model");
 
+// GET a user's plants
 router.get("/", (req, res, next) => {
     Plants.findByUserId(req.decodedToken.subject)
         .then(plants => {
@@ -8,5 +9,36 @@ router.get("/", (req, res, next) => {
         })
         .catch(next);
 })
+
+// GET a specific plant
+router.get("/:id", (req, res, next) => {
+    Plants.findByUserId(req.decodedToken.subject)
+        .then(plants => {
+            res.json(plants);
+        })
+        .catch(next);
+})
+
+
+// POST add a plant
+router.post("/", (req, res, next) => {
+    const plant = req.body
+    plant.user_id = req.decodedToken.subject
+    Plants.add(plant)
+        .then(plant => {
+            res.status(201).json(plant)
+        })
+        .catch(next)
+})
+
+
+
+//TODO PUT edit a plant
+
+
+
+//TODO DELETE remove a plant
+
+
 
 module.exports = router; 
